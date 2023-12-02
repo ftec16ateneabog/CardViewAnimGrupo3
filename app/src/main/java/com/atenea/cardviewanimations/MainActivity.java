e51b21f42ef83af3d62c5e7cdf6ffc1a3126a945
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-12-01 21:45:55 -0500

    add logic animation

--- app/src/main/java/com/atenea/cardviewanimations/MainActivity.java
+++ app/src/main/java/com/atenea/cardviewanimations/MainActivity.java
@@ -1,8 +1,14 @@
 package com.atenea.cardviewanimations;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView cardView = findViewById(R.id.myCardView);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        ImageView imageView = findViewById(R.id.myImageView);
+
+        Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale);
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.animation1);
+        Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.animation2);
+        Animation animation3 = AnimationUtils.loadAnimation(this, R.anim.animation3);
+
+        imageView.startAnimation(scaleAnimation);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(animation1);
+                // Aquí se coloca el código para lo que se quiera
+                // realizar después del clic en el botón
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardView.startAnimation(animation2);
+                // Aquí se coloca el código para lo que se quiera
+                // realizar después del clic en el botón
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.startAnimation(animation3);
+                // Aquí se coloca el código para lo que se quiera
+                // realizar después del clic en el botón
+            }
+        });
     }
 }
